8b7b98a7a37fbcf6720f00d74978c6f91663fb73
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-09-03 14:37:58 +0200

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import classes from './App.module.css';
+
+function App() {
+  return (
+    <div className={classes.mainContainer}>
+      <h1>Country Painters and Glazers Pvt Ltd</h1>
+    </div>
+  );
+}
+
+export default App;
