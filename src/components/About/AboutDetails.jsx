7f6530d0b8c1aeb749f1fe056952b4d2a1be03a7
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-09-06 23:31:45 +0200

    Add toogle details functionality

--- src/components/About/AboutDetails.jsx
+++ src/components/About/AboutDetails.jsx
@@ -1,32 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AboutUsDataArr } from '../Home/UI/Data';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 import classes from './AboutDetails.module.css';
 
 const AboutDetails = () => {
+  // Initialize state to track whether details are visible for each card
+  const [isDetailsVisible, setIsDetailsVisible] = useState(
+    AboutUsDataArr.map(() => false)
+  );
+
+  // Function to toggle the details visibility for a specific card
+  const toggleDetails = (index) => {
+    const updatedVisibility = [...isDetailsVisible];
+    updatedVisibility[index] = !updatedVisibility[index];
+    setIsDetailsVisible(updatedVisibility);
+  };
+
   return (
     <div className={classes.aboutDetailsContainer}>
-      {AboutUsDataArr.map((card) => (
-        <div className={classes.aboutCard}>
+      {AboutUsDataArr.map((card, index) => (
+        <div className={classes.aboutCard} key={index}>
           <div className={classes.cardHeader}>
             <h3 className={classes.cardName}>{card.name}</h3>
             <div className={classes.cardButtons}>
-              <button className={classes.toogleDetailsBtn}>
-                <AiOutlinePlus />
-              </button>
-              <button className={classes.toogleDetailsBtn}>
-                <AiOutlineMinus />
+              <button
+                className={classes.toogleDetailsBtn}
+                onClick={() => toggleDetails(index)} // Toggle details visibility
+              >
+                {isDetailsVisible[index] ? (
+                  <AiOutlineMinus />
+                ) : (
+                  <AiOutlinePlus />
+                )}
               </button>
             </div>
           </div>
-          <div className={classes.detailsCont}>
-            <img
-              src={card.image}
-              alt="AboutUsImage"
-              className={classes.aboutImage}
-            />
-            <p className={classes.cardDescrioption}>{card.description}</p>
-          </div>
+          {isDetailsVisible[index] && ( // Only render details if visibility is true
+            <div className={classes.detailsCont}>
+              <img
+                src={card.image}
+                alt="AboutUsImage"
+                className={classes.aboutImage}
+              />
+              <p className={classes.cardDescrioption}>{card.description}</p>
+            </div>
+          )}
         </div>
       ))}
     </div>
