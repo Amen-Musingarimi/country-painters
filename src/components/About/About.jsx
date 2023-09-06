d512034cd19df27281178855bc064c9d3a0e5f35
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-09-06 16:45:27 +0200

    Add about page first section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import classes from './About.module.css';
 import CurrentPage from '../helpers/CurrentPage';
+import { Link } from 'react-router-dom';
+import classes from './About.module.css';
 
 const About = () => {
   return (
     <div className={classes.aboutDiv}>
       <CurrentPage />
+      <div className={classes.pageWelcomeContainer}>
+        <h2 className={classes.welcomText}>
+          Welcome to our story, where passion meets craftsmanship.
+        </h2>
+        <div className={classes.borderTop}></div>
+        <p className={classes.sectionDescription}>
+          Contact our Customer Service representative to make an appointment and
+          to answer all your questions!
+        </p>
+        <Link to="/contact" className={classes.appointmentBtn}>
+          BOOK AN APPOINTMENT
+        </Link>
+      </div>
     </div>
   );
 };
